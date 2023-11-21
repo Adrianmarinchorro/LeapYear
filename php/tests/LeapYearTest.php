7590b618d8234035ba4197d9c5b191f27a43092c
adrian <adrian.marin.chorro@gmail.com>
2023-11-21 18:36:01 +0100

    apply DataProvider

--- php/tests/LeapYearTest.php
+++ php/tests/LeapYearTest.php
@@ -7,83 +7,42 @@ use PHPUnit\Framework\TestCase;
 
 class LeapYearTest extends TestCase
 {
-    /** @test */
-    public function given_1_then_it_is_not_leap(): void
-    {
-        $result = YearLeap::isLeap(1);
-
-        self::assertEquals(false, $result);
+    /**
+     * @dataProvider providerIsDivisibleBy4
+     * @dataProvider providerIsDivisibleBy100
+     * @dataProvider providerIsDivisibleBy400
+     */
+    public function test_given_a_year_then_return_bool(int $year, bool $expected) {
+        self::assertEquals($expected, YearLeap::isLeap($year));
     }
 
-    /** @test */
-    public function given_4_then_it_is_leap(): void
-    {
-        $result = YearLeap::isLeap(4);
 
-        self::assertEquals(true, $result);
+    public function providerIsDivisibleBy4() {
+
+        return array(
+            'given_1_then_it_is_not_leap' => array(1, false),
+            'given_4_then_it_is_leap' => array(4, true),
+            'given_8_then_it_is_leap' => array(8, true),
+            'given_12_then_it_is_leap' => array(12, true),
+        );
     }
 
-    /** @test */
-    public function given_8_then_it_is_leap(): void
-    {
-        $result = YearLeap::isLeap(8);
+    public function providerIsDivisibleBy100() {
 
-        self::assertEquals(true, $result);
+        return array(
+            'given_100_then_it_is_not_leap' => array(100, false),
+            'given_200_then_it_is_not_leap' => array(200, false),
+            'given_300_then_it_is_not_leap' => array(300, false),
+        );
     }
 
-    /** @test */
-    public function given_12_then_it_is_leap(): void
-    {
-        $result = YearLeap::isLeap(12);
+    public function providerIsDivisibleBy400() {
 
-        self::assertEquals(true, $result);
+        return array(
+            'given_400_then_it_is_leap' => array(400, true),
+            'given_800_then_it_is_leap' => array(800, true),
+            'given_1200_then_it_is_leap' => array(1200, true),
+        );
     }
 
-    /** @test */
-    public function given_100_then_it_is_not_leap(): void
-    {
-        $result = YearLeap::isLeap(100);
-
-        self::assertEquals(false, $result);
-    }
-
-    /** @test */
-    public function given_200_then_it_is_not_leap(): void
-    {
-        $result = YearLeap::isLeap(200);
-
-        self::assertEquals(false, $result);
-    }
-
-    /** @test */
-    public function given_300_then_it_is_not_leap(): void
-    {
-        $result = YearLeap::isLeap(300);
-
-        self::assertEquals(false, $result);
-    }
-
-    /** @test */
-    public function given_400_then_it_is_leap(): void
-    {
-        $result = YearLeap::isLeap(400);
-
-        self::assertEquals(true, $result);
-    }
-
-    /** @test */
-    public function given_800_then_it_is_leap(): void
-    {
-        $result = YearLeap::isLeap(800);
-
-        self::assertEquals(true, $result);
-    }
-
-    /** @test */
-    public function given_1200_then_it_is_leap(): void
-    {
-        $result = YearLeap::isLeap(1200);
-
-        self::assertEquals(true, $result);
-    }
 }
