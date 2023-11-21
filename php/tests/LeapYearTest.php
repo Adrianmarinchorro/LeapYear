16a84a823e163b10d8cc0483cc79441b2f026374
adrian <adrian.marin.chorro@gmail.com>
2023-11-21 17:51:10 +0100

    red: given_8_then_it_is_leap

--- php/tests/LeapYearTest.php
+++ php/tests/LeapYearTest.php
@@ -22,4 +22,12 @@ class LeapYearTest extends TestCase
 
         self::assertEquals(true, $result);
     }
+
+    /** @test */
+    public function given_8_then_it_is_leap(): void
+    {
+        $result = YearLeap::isLeap(8);
+
+        self::assertEquals(true, $result);
+    }
 }
