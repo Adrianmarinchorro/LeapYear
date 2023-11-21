655d406d2f4996073a9f1f769ba7aab303be5cb6
adrian <adrian.marin.chorro@gmail.com>
2023-11-21 18:05:00 +0100

    extract logic

--- php/src/YearLeap.php
+++ php/src/YearLeap.php
@@ -6,17 +6,9 @@ class YearLeap
 {
     public static function isLeap(int $year): bool
     {
-        if ($year === 300) {
-            return false;
-        }
-
-        if ($year === 200) {
-            return false;
-        }
-
-        if ($year === 100) {
-            return false;
-        }
+       if ($year % 100 === 0) {
+           return false;
+       }
 
         if ($year % 4 === 0) {
             return true;
